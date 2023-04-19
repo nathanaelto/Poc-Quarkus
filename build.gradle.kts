0586177fc11356d2d81afe35cb91599f0d59d79e
Nathanael TO <nathanaelto.pro@gmail.com>
2023-04-19 21:26:36 +0200

    Add SwaggerUi

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ val quarkusPlatformArtifactId: String by project
 val quarkusPlatformVersion: String by project
 
 dependencies {
+        implementation("io.quarkus:quarkus-smallrye-openapi")
     implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
     implementation("io.quarkus:quarkus-resteasy")
     implementation("io.quarkus:quarkus-jdbc-postgresql")
